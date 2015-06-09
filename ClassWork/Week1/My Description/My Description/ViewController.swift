6df0ffe9b954a1a382e531b207cce14afa118ca3
rhjr <royharringtonjr@gmail.com>
2015-06-08 21:21:59 -0400

    Build Bio App

---
+++ ClassWork/Week1/My Description/My Description/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Description
+//
+//  Created by Roy Harrington Jr. on 6/8/15.
+//  Copyright (c) 2015 Roy Harrington Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
