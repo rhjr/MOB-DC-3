0629dbd0d9510024b53068e008d6b2ebcf5c06bc
thomas <degry.thomas@gmail.com>
2015-06-17 18:29:44 -0400

    Lesson 05 code along

---
+++ Lessons/Lesson05/code-along/Lesson 5 Code Along/Lesson 5 Code Along/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Lesson 5 Code Along
+//
+//  Created by LOANER on 6/17/15.
+//  Copyright (c) 2015 Thomas Degry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
