ea57eb055a5336036f92e31bd1ed80e9624edfb7
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-07-06 20:16:18 -0400

    Added HW3 and Lesson10 solutions

--- Solutions/Lesson10/Lesson10-Delegation.playground/Contents.swift
+++ Solutions/Lesson10/Lesson10-Delegation.playground/Contents.swift
@@ -1,39 +1,155 @@
-protocol Delegate {
-    func shouldStartAWar()
-    func shouldMaintainPeace()
+// Protocol Review
+protocol Automotive {
+    func drive()
+    func honk()
+    func turnOnLights()
+}
+
+class Car: Automotive {
+    func drive() {
+        
+    }
+    
+    func honk() {
+        
+    }
+    
+    func turnOnLights() {
+        
+    }
+}
+
+class Train: Automotive {
+    func drive() {
+        
+    }
+    
+    func honk() {
+        
+    }
+    
+    func turnOnLights() {
+        
+    }
+}
+
+let honda = Car()
+let porsche = Car()
+let thomas = Train()
+
+var array = [Automotive]()
+array.append(honda)
+array.append(porsche)
+array.append(thomas)
+
+for v in array {
+    v.drive()
+}
+
+
+
+// Delegation
+
+/*****************Country.swift*******/
+protocol CountryDelegate {
+    func startAWar()
+    func makePeace()
 }
 
 class Country {
     
     var delegate: Spokesperson?
+    var delegates: [Spokesperson]?
     
     func causeProblems() {
+        
         if let delegate = delegate {
-            delegate.shouldStartAWar()
+            delegate.startAWar()
+        }
+        
+        if let arrayOfDelegates = delegates {
+            for d in arrayOfDelegates {
+                d.startAWar()
+            }
         }
     }
     
-    func solveProblems() {
+    func fixProblems() {
         if let delegate = delegate {
-            delegate.shouldStartAWar()
+            delegate.makePeace()
+        }
+        
+        if let arrayOfDelegates = delegates {
+            for d in arrayOfDelegates {
+                d.makePeace()
+            }
         }
     }
     
 }
+/***********************************/
 
-class Spokesperson: Delegate {
+class Person {
     
-    func shouldStartAWar() {
-        println("God created war so that Americans would learn geography. (Mark Twain)")
-        println("Let's go to war!")
-    }
-    
-    func shouldMaintainPeace() {
-        println("Peace begins with a smile. (Mother Theresa)")
-    }
 }
 
-let arthur = Spokesperson()
-let usa = Country()
-usa.delegate = arthur
-usa.causeProblems()
+class Spokesperson: Person, CountryDelegate {
+    
+    func startAWar() {
+        
+    }
+    
+    func makePeace() {
+        
+    }
+    
+}
+
+let handy = Spokesperson()
+let kris = Spokesperson()
+let kate = Spokesperson()
+let france = Country()
+france.delegates = [handy, kate, kris]
+france.causeProblems()
+
+
+
+//protocol Delegate {
+//    func shouldStartAWar()
+//    func shouldMaintainPeace()
+//}
+//
+//class Country {
+//    
+//    var delegate: Spokesperson?
+//    
+//    func causeProblems() {
+//        if let delegate = delegate {
+//            delegate.shouldStartAWar()
+//        }
+//    }
+//    
+//    func solveProblems() {
+//        if let delegate = delegate {
+//            delegate.shouldStartAWar()
+//        }
+//    }
+//    
+//}
+//
+//class Spokesperson: Delegate {
+//    
+//    func shouldStartAWar() {
+//        println("God created war so that Americans would learn geography. (Mark Twain)")
+//        println("Let's go to war!")
+//    }
+//    
+//    func shouldMaintainPeace() {
+//        println("Peace begins with a smile. (Mother Theresa)")
+//    }
+//}
+//
+//let arthur = Spokesperson()
+//let usa = Country()
+//usa.delegate = arthur
+//usa.causeProblems()
