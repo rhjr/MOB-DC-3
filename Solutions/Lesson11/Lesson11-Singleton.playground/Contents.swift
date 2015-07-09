48cd34f85cba783e4cac78a922de459b7c8396bb
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-07-08 20:49:54 -0400

    Sample code

--- Solutions/Lesson11/Lesson11-Singleton.playground/Contents.swift
+++ Solutions/Lesson11/Lesson11-Singleton.playground/Contents.swift
@@ -1,7 +1,21 @@
 // Define a Singleton
+
+class MyNormalClass {
+    internal var age: Int?
+}
+
+let a = MyNormalClass()
+a.age = 10
+let b = MyNormalClass()
+b.age
+
+//let b = a
+let check = (a === b)
+
 class MySingletonClass {
-    
+ 
     static let sharedInstance = MySingletonClass()
+    var age: Int?
     
     init() {
         println("Singleton, initialized!");
@@ -10,16 +24,20 @@ class MySingletonClass {
     func doSomething() {
         println("I'm doing something!")
     }
-    
 }
+
+
+let singleton = MySingletonClass.sharedInstance
 
 // One Singleton Instance
 let singletonA =  MySingletonClass.sharedInstance
 singletonA.doSomething()
+singletonA.age = 10
 
 // Another Singleton Instance
 let singletonB =  MySingletonClass.sharedInstance
 singletonB.doSomething()
+singletonB.age
 
 // Equivalence Check
 let equivalenceCheck = (singletonA === singletonB)
